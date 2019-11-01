674a3f223edaa4a204bffb6a4e49b7dd30fbe294
Your Name <keylung94@gmail.com>
2019-11-01 13:51:58 +0100

    added redirect

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import AboutPage from "./AboutPage";
 import Header from "./common/Header";
 import CoursesPage from "./CourserPage";
 import NotFoundPage from "./NotFoundPage";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
         <Route path="/" exact component={HomePage} />
         <Route path="/courses" component={CoursesPage} />
         <Route path="/about" component={AboutPage} />
+        <Redirect from="/about-page" to="/about" />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
