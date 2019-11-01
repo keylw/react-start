4aca122cc0006264cc9c09e2507b1886b00f0b23
Your Name <keylung94@gmail.com>
2019-11-01 10:25:39 +0100

    added a header navigator to the app

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
+import Header from "./common/Header";
 
 function App() {
-  const router = window.location.pathname;
-  if (router === "/about") return <AboutPage />;
-  return <HomePage />;
+  function getPage() {
+    const router = window.location.pathname;
+    if (router === "/about") return <AboutPage />;
+    return <HomePage />;
+  }
+
+  return (
+    <div className="container-fluid">
+      <Header />
+      {getPage()}
+    </div>
+  );
 }
 
 export default App;
