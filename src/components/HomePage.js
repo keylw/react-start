fbf80917108d7fb3fa12b7213fbd5c5e8291ccf7
Your Name <keylung94@gmail.com>
2019-11-01 13:12:26 +0100

    added link and navlink from react-router

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
     <div className="jumbotron">
       <h1>Pluralsigh Aministration</h1>
       <p>React, Flux, React Router for Responsive web apps.</p>
-      <a href="/about">About</a>
+      <Link to="/about" className="btn btn-primary">
+        About
+      </Link>
     </div>
   );
 }
